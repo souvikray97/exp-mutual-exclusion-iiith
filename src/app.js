047d2b9da2baa7b2f9013ec7091b30d31e89dfce
souvik ray <souvikray97@gmail.com>
2024-07-01 13:17:13 +0530

    added eventlistener to start button to create sim. if buffersize is zero/default,the start butotn will not work.

---
+++ src/app.js
@@ -0,0 +1,40 @@
+// Define the Process class with the required constructor
+class Process {
+  constructor(process = {}, clock = 3, value = undefined, state = "ready") {
+    this.process = process;
+    this.clock = clock;
+    this.value = value;
+    this.state = state;
+  }
+}
+
+document.getElementById("start").addEventListener("click", function () {
+  // Select the first radio button (buffer size 0) by default
+  const bufferSize = parseInt(
+    document.querySelector('input[name="radio"]:checked').value,
+    10,
+  );
+  console.log("Buffer size selected:", bufferSize);
+
+  if (bufferSize == 0) {
+    return;
+  }
+ 
+
+  const ol = document.getElementById("iteration");
+
+  // Clear existing items in the list
+  ol.innerHTML = "";
+
+  // Create an instance of the Process class
+  const processInstance = new Process("{}");
+
+  // Generate and append the div element with initial values using the Process instance
+  const div = document.createElement("div");
+  div.classList.add("flex", "border-b", "py-2");
+  div.innerHTML = `<div class="basis-1/4 pl-3 pr-5">${processInstance.process}</div>
+                   <div class="basis-1/4 px-2">${processInstance.clock}</div>
+                   <div class="basis-1/4 px-2">${processInstance.value}</div>
+                   <div class="basis-1/4 pr-3 pl-5 text-green-600">${processInstance.state}</div>`;
+  ol.appendChild(div);
+});
