1800e38b0af93526b401552e18c99273a60b5d8c
souvik ray <souvikray97@gmail.com>
2024-07-01 17:44:00 +0530

    clock update and state change

--- src/app.js
+++ src/app.js
@@ -58,7 +58,26 @@ put.addEventListener("keyup", function (event) {
             // Find the first empty spot in the process array to add the new value
             const emptyIndex = processInstance.process.indexOf("");
             if (emptyIndex !== -1) {
+                const ol = document.getElementById("iteration");
+               
+                    for (let i = 1; i <= 3; i++) {
+                        const newDiv = document.createElement("div");
+                        newDiv.classList.add("flex", "border-b", "py-2", "text-center");
+              
+                        newDiv.innerHTML = `<div class="basis-1/4 pl-3 pr-5">${processInstance.process}</div>
+                                            <div class="basis-1/4 px-2">${processInstance.clock - i}</div>
+                                            <div class="basis-1/4 px-2">${inputValue}</div>
+                                            <div class="basis-1/4 pr-3 pl-5 text-red-600">busy</div>`;
+              // Append each new div with a delay of 1 second
+                                            setTimeout(() => {
+                                                ol.appendChild(newDiv);
+                                              }, i*1000);
+                      }
+                
+                
+        
                 processInstance.process[emptyIndex] = inputValue;
+        
 
                 // Generate and append the div element with updated values
                 const div = document.createElement("div");
@@ -71,9 +90,12 @@ put.addEventListener("keyup", function (event) {
                        <div class="basis-1/4 px-2">${processInstance.value}</div>
                        <div class="basis-1/4 pr-3 pl-5 text-green-600">${processInstance.state}</div>`;
 
+                       
                 // Append the new div to the existing list (ol)
-                const ol = document.getElementById("iteration");
-                ol.appendChild(div);
+                // Add a 1-second delay before appending the main div with updated values
+                setTimeout(() => {
+                    ol.appendChild(div);
+                  },4000);
 
                 // Clear input field after processing
                 put.value = "";
